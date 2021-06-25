f2ab6bddb9f12be5bbe2f99d9aa741109b2dde82
zidniryi <zidnirydwan@gmail.com>
2021-06-25 11:08:46 +0700

    feate(users): Define root query

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,12 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
+
+const users = [
+  { id: "23", firstName: "zidni", age: 21 },
+  { id: "21", firstName: "kina", age: 7 },
+];
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -8,5 +14,18 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: GraphQLString,
     age: GraphQLInt,
+  },
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    users: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      },
+    },
   },
 });
